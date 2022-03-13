db9e24c284f8b00e947593fe83091c02f41fb50a
Osundwa Jeff <mulamajeff@tuta.io>
2022-03-13 12:45:12 -0700

    pos or neg/last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,18 +9,17 @@
 int main(void)
 {
 		int n;
-		int last;
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-		last = n % 10;
+		if (n > 0)
+			printf("%i is positive\n", n);
+		else if (n < 0)
+			printf("%i is negative\n", n);
+		else
+			printf("%i is zero\n", n);
 
-		if (last > 5)
-			printf("Last digit of %i is %i and is greater than 5\n", n, last);
-		else if (last == 0)
-			printf("Last digit of %i is %i and is 0\n", n, last);
-		else if (last < 6)
-			printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 
 		return (0);
 }
+
