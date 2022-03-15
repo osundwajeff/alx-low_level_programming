c9ffbe2e85aa8f34c8aab5537c223048c821efae
Osundwa Jeff <mulamajeff@tuta.io>
2022-03-15 13:08:04 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,13 +8,6 @@
 
 int _islower(int c)
 {
-	c = _islower('H');
-	_putchar(c + '0');
-	c = _islower('o');
-	_putchar(c + '0');
-	c = _islower(108);
-	_putchar(c + '0');
-	_putchar('\n');
-	return (0);
+		return (c >= 97 && c <= 122);
 
 }
