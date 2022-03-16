6033ea9bd6f70530971253f3bc1084d4c8f6b97a
Osundwa Jeff <mulamajeff@tuta.io>
2022-03-16 12:57:14 -0700

    0. Multiple mains

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* 0-main.c*/
+void positive_or_negative(int i);
+/* 2-main.c*/
+int largest_number(int a, int b, int c);
+/* 3-main.c*/
+void print_remaining_days(int month, int day, int year);
+/* -main.c*/
+int convert_day(int month, int day);
+/* -main.c*/
+void print_times_table(int n);
+
+#endif
