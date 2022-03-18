5dd6f158463c9d2653e3f4d267aa92231d365936
Osundwa Jeff <mulamajeff@tuta.io>
2022-03-18 06:17:40 -0700

    functions and nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,5 +5,23 @@
 int _isupper(int c);
 /* 1-is digit*/
 int _isdigit(int );
+/* 2-mul*/
+int mul(int a, int b);
+/* 3-print_numbers*/
+void print_numbers(void);
+/* 4-print_most numbers*/
+void print_most_numbers(void);
+/* 5-more_numbers*/
+void more_numbers(void);
+/* 6-print_line*/
+void print_line(int n);
+/* 7-print diagonal*/
+void print_diagonal(int n);
+/* 8-print_square*/
+void print_square(int size);
+/* 9-print_triangle*/
+void print_triangle(int size);
+/* 10-print_number*/
+void print_number(int n);
 
 #endif
