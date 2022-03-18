896299e848260b6e1ff44a4acedfc757be064f5e
Osundwa Jeff <mulamajeff@tuta.io>
2022-03-18 05:31:29 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+More functions on Nested Loops
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/* 0-isupper*/
+int _isupper(int c);
+/* 1-is digit*/
+int _isdigit(int );
+
+#endif
