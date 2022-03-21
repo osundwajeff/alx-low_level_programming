9dbc2bb35b0f9ecb9f5bfa8813dca546fe3c9c27
Osundwa Jeff <mulamajeff@tuta.io>
2022-03-21 14:20:19 -0700

    swap.c file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int - function that swaps the values of two integers 
+ * swap_int - function that swaps the values of two integers
  * @a: input 1
  * @b: input 2
  * Return: integer
