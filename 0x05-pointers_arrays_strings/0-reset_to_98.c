cc465181498e7dd203b8d38affc5b1a3013875ff
Osundwa Jeff <mulamajeff@tuta.io>
2022-03-21 14:00:11 -0700

    minor change

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,8 @@
 /*
  * reset_to_98 - a function that takes a pointer to an int as a parameter
  * and updates the value to 98
- * Return: 0
+ * @n: input
+ * Return: n
  */
 void reset_to_98(int *n)
 {
