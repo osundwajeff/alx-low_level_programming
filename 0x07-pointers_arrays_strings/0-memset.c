4bc936139fc114ca952f50ab806cee2f61b3793b
Osundwa Jeff <mulamajeff@tuta.io>
2022-03-28 04:56:37 -0700

    memory test

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,17 +5,16 @@
  *  with the constant byte @b
  * @s: pointer to memory area
  * @b: constant byte to fill area
- * @c: number of bytes to be filled
+ * @n: number of bytes to be filled
  *
  * Return: pointer to memory area filled by @s
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(void *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value =c;
+	unsigned int i;
 
-	for(index = 0; index < n; index++)
-		memory[index] = value;
+	for(i = 0; i < n; i++)
+		s[i] = b;
 
-	return (memory);
+	return (s);
 }
