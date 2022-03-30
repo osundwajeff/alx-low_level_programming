dd7861530333d145391057051b75904f99cd4667
Osundwa Jeff <mulamajeff@tuta.io>
2022-03-30 14:09:11 -0700

    changes on _strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,8 +6,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\n')
+	if (*s == '\0')
 		return (0);
 	else
-		return (1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 }
