076f0f05e0ecdd7348ab82ba476074a52691d9ab
Osundwa Jeff <mulamajeff@tuta.io>
2022-03-30 14:33:21 -0700

    Recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#inlcude "main.h"
+/**
+ * _pow_recursion - function to get x raised to y
+ * @x: input
+ * @y: input
+ * Return: return power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
