a1cf25295a0da79c8aefd5cb87221d40cb5c2edf
Osundwa Jeff <mulamajeff@tuta.io>
2022-04-01 13:38:15 -0700

    whatsmyname commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints its name + \n
+ * @argc: number of arguments types
+ * Qargv: array pointing to arguments
+ * Return: return (0)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc]);
+	return (0);
+}
