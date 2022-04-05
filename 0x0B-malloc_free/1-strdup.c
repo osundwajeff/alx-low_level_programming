d7873320242e4b3c3e795f57b87a264979c918a8
Osundwa Jeff <mulamajeff@tuta.io>
2022-04-05 13:29:35 -0700

    added 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	int i, end;
+	char *arr;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (end = 0; end <= *str; end++)
+	{
+	}
+
+	end += 1;
+	arr = malloc(sizeof(char) * end);
+
+	for (i = 0; i < end; i++)
+		arr[i] = str[i];
+
+	if (arr == NULL)
+		return (NULL);
+	return (arr);
+}
