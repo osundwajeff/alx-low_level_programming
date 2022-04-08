fa3ac61c1de87bccaab8b53c493648c3d5e9ec69
Osundwa Jeff <mulamajeff@tuta.io>
2022-04-08 03:45:37 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - allocates memory
+ * @nmemb: number of elements in array
+ * @size: bytes
+ * Return: returns(pointer)
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		arr[i] = 0;
+	return (arr);
+}
