0872dcbdaf34b64696096add825da9b2e30db4e7
Osundwa Jeff <mulamajeff@tuta.io>
2022-04-08 02:13:06 -0700

    0-malloc_checked file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - check memory allocation
+ * @b: input
+ *
+ * Return: return (pointer to allocated memory)
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+	
+	if (a = NULL)
+		exit(98);
+
+	return (a);
+}
