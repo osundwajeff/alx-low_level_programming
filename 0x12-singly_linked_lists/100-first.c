e925234fd43320079f11438384c38d8deba707ff
osundwajeff <mulamajeff@tuta.io>
2022-04-21 16:50:55 -0700

    fix ;

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,5 +6,5 @@ void __attribute__((constructor))start(void);
  */
 void start(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n")
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
