a453a0ffa866ccd461ed783f0dfc666abbc23aa5
osundwajeff <mulamajeff@tuta.io>
2022-04-25 12:52:39 -0700

    added 4-free_listint

---
+++ 0x13-more_singly_linked_lists/tests/4-free_listint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint - function that frees a listint_t list
+ * @head: node
+ * Return: 0
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	if (head->next == NULL)
+		free_listint(head->next);
+	free(head);
+}
