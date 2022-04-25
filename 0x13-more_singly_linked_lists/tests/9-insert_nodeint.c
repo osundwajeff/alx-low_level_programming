3a314d16f295f493af269a593acf0e86309b329c
osundwajeff <mulamajeff@tuta.io>
2022-04-25 13:55:12 -0700

    added 9-insert_nodeint.c files

---
+++ 0x13-more_singly_linked_lists/tests/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts a new node at a given
+ * position.
+ * @head: node
+ * @idx: index
+ * @n: integer
+ * Return: 0
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int cnt = 0;
+
+	listint_t *new;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (NULL);
+
+	temp = *head;
+
+	while (temp != NULL)
+	{
+		if (cnt == idx - 1)
+		{
+			new = malloc(sizeof(listint_t));
+			if (new == NULL)
+			{
+				return (NULL);
+			}
+			else
+			{
+				new->n = n;
+				new->next = temp->next;
+				temp->next = new;
+				return (new);
+			}
+		}
+		if (temp->next != NULL)
+			temp = temp->next;
+		cnt++;
+	}
+	return (NULL);
+}
