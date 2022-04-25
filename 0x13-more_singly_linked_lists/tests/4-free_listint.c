acd755478220f377da700b445f5bef669cbff09c
osundwajeff <mulamajeff@tuta.io>
2022-04-25 12:59:02 -0700

    added 4-free_listint

--- 0x13-more_singly_linked_lists/tests/4-free_listint.c
+++ 0x13-more_singly_linked_lists/tests/4-free_listint.c
@@ -11,7 +11,9 @@ void free_listint(listint_t *head)
 	if (head == NULL)
 		return;
 
-	if (head->next == NULL)
-		free_listint(head->next);
-	free(head);
+	while (head)
+	{
+		head = head->next;
+		free(head);
+	}
 }
