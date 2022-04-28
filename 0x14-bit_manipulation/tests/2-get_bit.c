1d6ad190b82891e90e21c51ef171a36fa2e0fcb9
osundwajeff <mulamajeff@tuta.io>
2022-04-28 07:20:39 -0700

    added 2-get_bit.c

---
+++ 0x14-bit_manipulation/tests/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * get_bit - returns value of abit at agiven index
+ * index: index
+ * n: integer
+ *
+ * Return: value of bit at index or -1 if an error occured.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
+}
