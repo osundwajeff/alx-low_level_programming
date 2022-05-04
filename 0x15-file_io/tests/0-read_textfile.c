e8b546fe7b6f13d15c3cb606106e8031c59f61cd
osundwajeff <mulamajeff@tuta.io>
2022-05-04 08:41:24 -0700

    minor fix

--- 0x15-file_io/tests/0-read_textfile.c
+++ 0x15-file_io/tests/0-read_textfile.c
@@ -3,7 +3,7 @@
  * read_textfile - reads a text file and prints it to the
  * POSIX standard output.
  * @filename: filename
- * @letter: characters
+ * @letters: characters
  * Return: number of letter it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
