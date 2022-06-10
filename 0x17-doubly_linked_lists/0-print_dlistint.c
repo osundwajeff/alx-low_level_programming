2dde632d9441968fa4fcc557c79f7e6f2a361b7d
osundwajeff <mulamajeff@tuta.io>
2022-06-10 16:57:01 -0700

    correction

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,11 +11,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		node++;
 	}
 
 	return (nodes);
