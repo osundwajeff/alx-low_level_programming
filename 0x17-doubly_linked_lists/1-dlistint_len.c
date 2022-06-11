72681a323e9dbfd9c889abeec62c4e15043b6208
osundwajeff <mulamajeff@tuta.io>
2022-06-10 17:02:03 -0700

    corrections

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,12 +8,20 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count = 0;
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
 
 	while (h != NULL)
 	{
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 
 	return (count);
