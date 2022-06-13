2b6f5888e09d7a86e12429a1ff269831429be55c
osundwajeff <mulamajeff@tuta.io>
2022-06-13 14:50:19 -0700

    gm

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/cheatme.so https://github.com/osundwajeff/alx-low_level_programming/blob/master/0x18-dynamic_libraries/cheatme.so
-export LD_PRELOAD=/tmp/cheatme.so
+wget -q -O /tmp/gm https://github.com/holbertonschool/0x18.c/blob/master/gm
+export LD_PRELOAD=/tmp/gm
