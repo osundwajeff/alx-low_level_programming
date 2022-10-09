15a654d0a8d590f6da6daa6b54e7d62e6f7db529
osundwajeff <mulamajeff@tuta.io>
2022-10-08 17:03:14 -0700

    prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,6 +7,8 @@
 int linear_search(int *array, size_t size, int value);
 
 /* binary search */
+void print_array(int *array, size_t l, size_t r);
+int binary_search_index(int *array, size_t l, size_t r, int value);
 int binary_search(int *array, size_t size, int value);
 
 #endif /*search_algos_h*/
